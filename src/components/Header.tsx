be0dd3dba3962a926f61fb687d8c42e6f2c9f1e3
VaultDweller13 <grognak13@gmail.com>
2023-06-13 01:32:14 +0400

    feat: add Home page text content

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export const Header = () => {
   return (
     <>
-      <h1>#Vanlife</h1>
+      <p>#Vanlife</p>
       <nav>
         <Link to="/about">About</Link>
         <Link to="/vans">Vans</Link>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,14 @@ export const Home = () => {
   return (
     <>
       <Header />
+      <main>
+        <h1>You got the travel plans, we got the travel vans.</h1>
+        <p>
+          Add adventure to your life by joining the #vanlife movement. Rent the
+          perfect van to make your perfect road trip.
+        </p>
+        <button>Find your van</button>
+      </main>
     </>
   );
 };
