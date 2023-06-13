44a8f9be790e7d2986dc6d9239baf3a8d09c0464
VaultDweller13 <45403328+VaultDweller13@users.noreply.github.com>
2023-06-13 11:47:00 +0400

    feat: add Home page main content styles

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,23 @@
+import { Footer } from '../../components/Footer';
+import { Header } from '../../components/Header';
+
+import styles from './Home.module.css';
+
+export const Home = () => {
+  return (
+    <>
+      <Header />
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          You got the travel plans, we got the travel vans.
+        </h1>
+        <p className={styles.content}>
+          Add adventure to your life by joining the #vanlife movement. Rent the
+          perfect van to make your perfect road trip.
+        </p>
+        <button className={styles.button}>Find your van</button>
+      </main>
+      <Footer />
+    </>
+  );
+};
