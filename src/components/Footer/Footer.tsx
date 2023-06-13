a9d5c70249d19bae9a36590836e7de890fa81817
VaultDweller13 <45403328+VaultDweller13@users.noreply.github.com>
2023-06-13 11:52:11 +0400

    feat: add footer styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,8 @@
+import styles from './Footer.module.css';
+
 export const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.footer}>
       <p>Ⓒ 2022 #VANLIFE</p>
     </footer>
   );
