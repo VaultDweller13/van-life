793e83e8ce30d7e2defe0cd355f674ee13720e53
VaultDweller13 <grognak13@gmail.com>
2023-06-17 00:26:12 +0400

    feat: add styling to header's nav current link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,22 +1,33 @@
-import { Link } from 'react-router-dom';
+import { useCallback } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 import styles from './Header.module.css';
 
+type ClassNameProps = {
+  isActive: boolean;
+};
+
 export const Header = () => {
+  const handleActive = useCallback(
+    ({ isActive }: ClassNameProps) =>
+      isActive ? `${styles.link} ${styles.active}` : styles.link,
+    []
+  );
+
   return (
     <header className={styles.header}>
       <Link to="/" className={styles.title}>
         #Vanlife
       </Link>
       <nav className={styles.nav}>
-        <Link to="/host" className={styles.link}>
+        <NavLink to="/host" className={handleActive}>
           Host
-        </Link>
-        <Link to="/about" className={styles.link}>
+        </NavLink>
+        <NavLink to="/about" className={handleActive}>
           About
-        </Link>
-        <Link to="/vans" className={styles.link}>
+        </NavLink>
+        <NavLink to="/vans" className={handleActive}>
           Vans
-        </Link>
+        </NavLink>
       </nav>
     </header>
   );
