bd4f1630df9f610c4fab9f624647b622ae837f99
VaultDweller13 <grognak13@gmail.com>
2023-06-17 12:58:29 +0400

    feat: add host pages active link styling

--- src/layouts/HostLayout.tsx
+++ src/layouts/HostLayout.tsx
@@ -1,13 +1,31 @@
-import { Link, Outlet } from 'react-router-dom';
+import { useCallback } from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
+
 import styles from './HostLayout.module.css';
 
+type ClassNameProps = {
+  isActive: boolean;
+};
+
 export const HostLayout = () => {
+  const handleActive = useCallback(
+    ({ isActive }: ClassNameProps) =>
+      isActive ? `${styles.link} ${styles.active}` : styles.link,
+    []
+  );
+
   return (
     <>
       <nav className={styles.nav}>
-        <Link to="/host">Dashboard</Link>
-        <Link to="/host/income">Income</Link>
-        <Link to="/host/reviews">Reviews</Link>
+        <NavLink to="/host" end className={handleActive}>
+          Dashboard
+        </NavLink>
+        <NavLink to="/host/income" className={handleActive}>
+          Income
+        </NavLink>
+        <NavLink to="/host/reviews" className={handleActive}>
+          Reviews
+        </NavLink>
       </nav>
       <Outlet />
     </>
