104c7ef13671721d7c73a873708e036feb45e9d6
VaultDweller13 <grognak13@gmail.com>
2023-06-18 23:37:10 +0400

    feat: add HostVans and HostVanInfo pages without styles

--- src/pages/Host/HostVans/HostVans.tsx
+++ src/pages/Host/HostVans/HostVans.tsx
@@ -1,7 +1,33 @@
+import { useState, useEffect } from 'react';
+import styles from './HostVans.module.css';
+import { VanData } from '../../../types';
+import { HostVanCard } from '../../../components/HostVanCard/HostVanCard';
+
 export const HostVans = () => {
+  const [vansData, setVansData] = useState<VanData[]>();
+  const [error, setError] = useState<string>();
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('/api/host/vans');
+
+      if (response.ok) {
+        const data = (await response.json()).vans;
+        setVansData(data);
+      } else {
+        setError(`${response.status}: ${response.statusText}`);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  const vans = vansData?.map((data) => <HostVanCard {...data} />);
+
   return (
-    <main>
-      <h1>Vans</h1>
+    <main className={styles.main}>
+      <h1 className={styles.title}>Your listed vans</h1>
+      {vans}
     </main>
   );
 };
