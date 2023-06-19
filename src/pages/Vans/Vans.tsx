dcecf50f2566f258f6df375fb1a2920e61152c90
VaultDweller13 <grognak13@gmail.com>
2023-06-19 22:00:36 +0400

    feat: implement filtering with search params

--- src/pages/Vans/Vans.tsx
+++ src/pages/Vans/Vans.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 
 import { Card } from '../../components';
 import { TypeButton } from '../../components/Buttons';
@@ -7,20 +7,18 @@ import { VanData } from '../../types';
 import styles from './Vans.module.css';
 
 export const Vans = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const filterType = searchParams.get('type');
+
   const filterOptions = ['simple', 'luxury', 'rugged'] as const;
   const filterButtons = filterOptions.map((option, index) => (
-    <TypeButton isDefault={true} type={option} key={index}>
-      {option}
-    </TypeButton>
-  ));
-  const [vansData, setVansData] = useState<VanData[]>();
-  const [error, setError] = useState<string>();
-
-  const vans = vansData?.map((data) => (
-    <Link to={`${data.id}`} className={styles.card} key={data.id}>
-      <Card {...data} />
+    <Link to={`?type=${option}`} key={index}>
+      <TypeButton isDefault={true} type={option}>
+        {option}
+      </TypeButton>
     </Link>
   ));
+  const [vansData, setVansData] = useState<VanData[]>();
 
   useEffect(() => {
     async function fetchData() {
@@ -29,13 +27,29 @@ export const Vans = () => {
       if (response.ok) {
         const data = (await response.json()).vans;
         setVansData(data);
-      } else {
-        setError(`${response.status}: ${response.statusText}`);
       }
     }
 
     fetchData();
   }, []);
+
+  if (!vansData) {
+    return (
+      <main className={styles.main}>
+        <h2>Loading...</h2>
+      </main>
+    );
+  }
+
+  const filteredVans = filterType
+    ? vansData?.filter((data) => data.type === filterType)
+    : vansData;
+
+  const vans = filteredVans.map((data) => (
+    <Link to={`${data.id}`} className={styles.card} key={data.id}>
+      <Card {...data} />
+    </Link>
+  ));
 
   return (
     <>
@@ -43,9 +57,11 @@ export const Vans = () => {
         <h1 className={styles.title}>Explore our van options</h1>
         <section className={styles.filters}>
           <div className={styles['filter-buttons']}>{filterButtons}</div>
-          <button className={styles['button-clear']}>Clear filters</button>
+          <Link to=".">
+            <button className={styles['button-clear']}>Clear filters</button>
+          </Link>
         </section>
-        <section className={styles.vans}>{!error && vans}</section>
+        <section className={styles.vans}>{vans}</section>
       </main>
     </>
   );
